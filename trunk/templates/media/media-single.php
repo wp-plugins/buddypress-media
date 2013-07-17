a1bdc79ee5f4e546f1bbd72e604d7b9b4abb32aa
faishal <faishal@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-07-17 08:24:14 +0000

    Tagging version 3.0.4
    git-svn-id: https://plugins.svn.wordpress.org/buddypress-media/tags/3.0.4@742134 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ trunk/templates/media/media-single.php
@@ -0,0 +1,41 @@
+<div class="rtmedia-container rtmedia-single-container">
+    <div class="row">
+        <?php if (have_rtmedia()) : rtmedia(); ?>
+
+            <div id="rtmedia-single-media-container" class="rtmedia-single-media columns large-9">
+
+                <?php rtmedia_title(); ?>
+
+
+                <div class="rtmedia-media" id ="rtmedia-media-<?php echo rtmedia_id(); ?>">
+                    <?php rtmedia_media(true); ?>
+                </div>
+
+            </div>
+            <div class="rtmedia-single-meta columns large-3">
+
+                <?php rtmedia_description(); ?>
+
+                <div class="rtmedia-item-actions">
+                    <?php rtmedia_actions(); ?>
+                </div>
+
+                <?php if (rtmedia_comments_enabled()) { ?>
+                    <div class="rtmedia-item-comments row">
+                        <div class="large-12 columns">
+                            <h2>Comments</h2>
+                            <div class="rtmedia-comments-container">
+                                <?php rtmedia_comments(); ?>
+                            </div>
+                            <?php rtmedia_comment_form(); ?>
+                        </div>
+                    </div>
+                <?php } ?>
+            </div>
+
+        <?php else: ?>
+            <p><?php echo __("Oops !! There's no media found for the request !!", "rtmedia"); ?></p>
+        <?php endif; ?>
+
+    </div>
+</div>
