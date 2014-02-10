331615f17e095e0e43d71ff07bc37c5688a4b9fb
faishal <faishal@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-10 07:55:38 +0000

    Version 3.6 released with API support
    git-svn-id: https://plugins.svn.wordpress.org/buddypress-media/trunk@854592 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
   Plugin Name: rtMedia for WordPress, BuddyPress and bbPress
   Plugin URI: http://rtcamp.com/buddypress-media/?utm_source=dashboard&utm_medium=plugin&utm_campaign=buddypress-media
   Description: This plugin adds missing media rich features like photos, videos and audio uploading to BuddyPress which are essential if you are building social network, seriously!
-  Version: 3.5.2.1
+  Version: 3.6
   Author: rtCamp
   Text Domain: rtmedia
   Author URI: http://rtcamp.com/?utm_source=dashboard&utm_medium=plugin&utm_campaign=buddypress-media
@@ -55,7 +55,7 @@ if ( ! defined ( 'RTMEDIA_BASE_NAME' ) ) {
 
 if ( ! defined ( 'RTMEDIA_BOWER_COMPONENTS_URL' ) ) {
     /**
-     * The url to the foundation resources 
+     * The url to the foundation resources
      *
      */
     define ( 'RTMEDIA_BOWER_COMPONENTS_URL', RTMEDIA_URL . 'app/assets/sass/bower_components/foundation/' );
@@ -102,6 +102,7 @@ function rtmedia_autoloader ( $class_name ) {
         'app/main/template/' . $class_name . '.php',
         'app/log/' . $class_name . '.php',
         'app/importers/' . $class_name . '.php',
+        'app/main/controllers/api/' . $class_name . '.php',
     );
     foreach ( $rtlibpath as $path ) {
         $path = RTMEDIA_PATH . $path;
