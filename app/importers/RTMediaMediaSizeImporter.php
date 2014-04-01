8a21e8d0936b61828c1897f3092bff1d0015a33a
faishal <faishal@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-01 07:29:23 +0000

    version 3.6.11 released
    git-svn-id: https://plugins.svn.wordpress.org/buddypress-media/trunk@885501 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- app/importers/RTMediaMediaSizeImporter.php
+++ app/importers/RTMediaMediaSizeImporter.php
@@ -18,6 +18,7 @@ class RTMediaMediaSizeImporter {
 		add_action( 'wp_ajax_rtmedia_media_size_import', array( $this, "rtmedia_media_size_import" ) );
 		add_action( 'admin_init', array( $this, 'add_admin_notice' ) );
 		add_action( 'admin_menu', array( $this, 'menu' ), 10 );
+		add_action( 'wp_ajax_rtmedia_hide_media_size_import_notice', array( $this, "rtmedia_hide_media_size_import_notice" ) );
 	}
 
 	function menu() {
@@ -30,12 +31,25 @@ class RTMediaMediaSizeImporter {
 		return $admin_pages;
 	}
 
+	function rtmedia_hide_media_size_import_notice() {
+		if( rtmedia_update_site_option( "rtmedia_hide_media_size_import_notice", true ) ) {
+			echo '1';
+		} else {
+			echo '0';
+		}
+		wp_die();
+	}
+
 	function add_admin_notice() {
 		$pending = $this->get_pending_count();
 		if ( $pending < 0 ){
 			$pending = 0;
 		}
 		rtmedia_update_site_option( "rtmedia_media_size_import_pending_count", $pending );
+		$hide_admin_option = rtmedia_get_site_option( 'rtmedia_hide_media_size_import_notice' );
+		if( $hide_admin_option ) {
+			return;
+		}
 		if ( $pending > 0 ){
 			if ( ! ( isset ( $_REQUEST[ "page" ] ) && $_REQUEST[ "page" ] == "rtmedia-migration-media-size-import" ) ){
 				$site_option = get_site_option( "rtmedia_media_size_import_notice" );
@@ -49,8 +63,8 @@ class RTMediaMediaSizeImporter {
 
 	function add_rtmedia_media_size_import_notice() {
 		if ( current_user_can( 'manage_options' ) ){
-			$this->create_notice( "<p><strong>rtMedia</strong>: <a href='" . admin_url( "admin.php?page=rtmedia-migration-media-size-import&force=true" ) . "'>Click Here</a> to import media sizes.  <a href='#' onclick='rtmedia_hide_media_size_import_notice()' style='float:right'>" . __( "Hide" ) . "</a> </p>" );
-			?>
+			$this->create_notice( "<p><strong>rtMedia</strong>: Database table structure for rtMedia has been updated. Please <a href='" . admin_url( "admin.php?page=rtmedia-migration-media-size-import&force=true" ) . "'>Click Here</a> to import media sizes. <a href='#' onclick='rtmedia_hide_media_size_import_notice()' style='float:right'>" . __( "Hide" ) . "</a>  </p>" );
+		?>
 			<script type="text/javascript">
 				function rtmedia_hide_media_size_import_notice() {
 					var data = {action: 'rtmedia_hide_media_size_import_notice'};
@@ -76,18 +90,18 @@ class RTMediaMediaSizeImporter {
 		$done    = $total - $pending;
 		?>
 		<div class="wrap">
-			<h2>rtMedia Media Size Import</h2>
-			<?php
-			echo '<span class="pending">' . rtmedia_migrate_formatseconds( $total - $done ) . '</span><br />';
+			<h2>rtMedia: Import Media Size</h2>
+		<?php
+			echo '<span class="pending">' . rtmedia_migrate_formatseconds( $total - $done ) . ' (estimated)</span><br />';
 			echo '<span class="finished">' . $done . '</span>/<span class="total">' . $total . '</span>';
 			echo '<img src="images/loading.gif" alt="syncing" id="rtMediaSyncing" style="display:none" />';
 
 			$temp = $prog->progress( $done, $total );
 			$prog->progress_ui( $temp, true );
-			?>
+		?>
 			<script type="text/javascript">
-				var false_count = 0;
-				var curr_done = 0;
+				var fail_id = new Array();
+				var ajax_data;
 				jQuery( document ).ready( function ( e ) {
 					jQuery( "#toplevel_page_rtmedia-settings" ).addClass( "wp-has-current-submenu" )
 					jQuery( "#toplevel_page_rtmedia-settings" ).removeClass( "wp-not-current-submenu" )
@@ -97,17 +111,19 @@ class RTMediaMediaSizeImporter {
 					if ( db_total < 1 )
 						jQuery( "#submit" ).attr( 'disabled', "disabled" );
 				} )
-				function db_start_migration( db_done, db_total ) {
+				function db_start_migration( db_done, db_total, last_id ) {
 
 					if ( db_done < db_total ) {
 						jQuery( "#rtMediaSyncing" ).show();
+						ajax_data = {
+							"action": "rtmedia_media_size_import",
+							"done": db_done,
+							"last_id" : last_id
+						}
 						jQuery.ajax( {
 							url: rtmedia_admin_ajax,
 							type: 'post',
-							data: {
-								"action": "rtmedia_media_size_import",
-								"done": db_done
-							},
+							data: ajax_data,
 							success: function ( sdata ) {
 
 								try {
@@ -127,17 +143,10 @@ class RTMediaMediaSizeImporter {
 									jQuery( 'span.finished' ).html( done );
 									jQuery( 'span.total' ).html( total );
 									jQuery( 'span.pending' ).html( data.pending );
-									if( curr_done == done ) {
-										false_count++;
-									} else {
-										false_count = 0;
+									if( data.imported === false ) {
+										fail_id.push(data.media_id);
 									}
-									curr_done = done;
-									if( false_count > 5 ) {
-										rtm_show_file_error( done, total );
-									} else {
-										db_start_migration( done, total );
-									}
+									db_start_migration( done, total, parseInt( data.media_id ) );
 								} else {
 									alert( "Migration completed." );
 									jQuery( "#rtMediaSyncing" ).hide();
@@ -150,18 +159,23 @@ class RTMediaMediaSizeImporter {
 						} );
 					} else {
 						alert( "Migration completed." );
+						if( fail_id.length > 0 ) {
+							rtm_show_file_error();
+						}
 						jQuery( "#rtMediaSyncing" ).hide();
 					}
 				}
-				function rtm_show_file_error( done, total ) {
-					jQuery( 'span.pending' ).html( "File size of " + ( total - done ) + " file(s) are not imported. Don't worry, you can end importing media size now :)" );
-					jQuery( "#rtMediaSyncing" ).hide();
+				function rtm_show_file_error() {
+					jQuery( 'span.pending' ).html( "Media with ID: " + fail_id.join() + " can not be imported. Please check your server error log for more details. Don't worry, you can end importing media size now :)" );
+//					var data = {action: 'rtmedia_hide_media_size_import_notice'};
+//					jQuery.post( ajaxurl, data, function ( response ) { } );
+//					jQuery( "#rtMediaSyncing" ).hide();
 				}
 				var db_done = <?php echo $done; ?>;
 				var db_total = <?php echo $total; ?>;
 				jQuery( document ).on( 'click', '#submit', function ( e ) {
 					e.preventDefault();
-					db_start_migration( db_done, db_total );
+					db_start_migration( db_done, db_total, 0 );
 					jQuery( this ).attr( 'disabled', 'disabled' );
 				} );
 			</script>
@@ -173,10 +187,13 @@ class RTMediaMediaSizeImporter {
 	<?php
 	}
 
-	function get_pending_count() {
+	function get_pending_count( $media_id = false ) {
 		global $wpdb;
 		$rtmedia_model = new RTMediaModel();
 		$query_pending = "SELECT COUNT(*) as pending from {$rtmedia_model->table_name} where file_size IS NULL AND media_type in ('photo','video','document','music','other')";
+		if( $media_id ) {
+			$query_pending = "SELECT COUNT(*) as pending from {$rtmedia_model->table_name} where file_size IS NULL AND media_type in ('photo','video','document','music','other') AND id > '" . $media_id . "'";
+		}
 		$pending_count = $wpdb->get_results( $query_pending );
 		if ( $pending_count && sizeof( $pending_count ) > 0 ){
 			return $pending_count[ 0 ]->pending;
@@ -201,32 +218,41 @@ class RTMediaMediaSizeImporter {
 		global $wpdb;
 		$rtmedia_model = new RTMediaModel();
 		$get_media_sql = "SELECT * from {$rtmedia_model->table_name} where file_size is NULL and media_type in ('photo','video','document','music','other') order by id limit " . $limit;
+		if( isset( $_REQUEST['last_id'] ) ) {
+			$lastid = $_REQUEST['last_id'];
+		}
 		if ( $lastid ){
 			$get_media_sql = "SELECT * from {$rtmedia_model->table_name} where id > '" . $lastid . "' AND file_size is NULL and media_type in ('photo','video','document','music','other') order by id limit " . $limit;
 		}
 		$result = $wpdb->get_results( $get_media_sql );
 		if ( $result && sizeof( $result ) > 0 ){
-			$this->migrate_single_media( $result[ 0 ] );
+			$migrate = $this->migrate_single_media( $result[ 0 ] );
 		}
-		$this->return_migration();
+		$this->return_migration( $result[ 0 ], $migrate );
 	}
 
 	function migrate_single_media( $result ) {
 		global $wpdb;
 		$rtmedia_model = new RTMediaModel();
 		$attached_file = get_attached_file( $result->media_id );
-		if ( ! file_exists( $attached_file ) ){
-			$this->rtmedia_media_size_import( $result->id );
+		$return = true;
+		if ( file_exists( $attached_file ) ){
+			$file_size = filesize( $attached_file );
+		} else {
+			$file_size = '0';
+			error_log( 'rtMedia size importer: file not exist. Media ID: '.$result->id.', File: '.$attached_file );
+			$return = false;
+			return false;
 		}
-		$file_size = filesize( $attached_file );
 		$post      = get_post( $result->media_id );
 		$post_date = $post->post_date;
 		$rtmedia_model->update( array( 'upload_date' => $post_date, 'file_size' => $file_size ), array( 'id' => $result->id ) );
+		return $return;
 	}
 
-	function return_migration() {
+	function return_migration( $media, $migrate = true ) {
 		$total   = $this->get_total_count();
-		$pending = $this->get_pending_count();
+		$pending = $this->get_pending_count( $media->id );
 		$done    = $total - $pending;
 		if ( $pending < 0 ){
 			$pending = 0;
@@ -237,7 +263,7 @@ class RTMediaMediaSizeImporter {
 		}
 		rtmedia_update_site_option( 'rtmedia_media_size_import_pending_count', $pending );
 		$pending_time = rtmedia_migrate_formatseconds( $pending ). " (estimated)";
-		echo json_encode( array( "status" => true, "done" => $done, "total" => $total, "pending" => $pending_time ) );
+		echo json_encode( array( "status" => true, "done" => $done, "total" => $total, "pending" => $pending_time, "media_id" => $media->id, "imported" => $migrate ) );
 		die();
 	}
 }
