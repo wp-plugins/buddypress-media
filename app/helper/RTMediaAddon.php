0441c77ce8395ee0673d9510aa54d4d824c82aff
rtcamp <rtcamp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-25 06:51:48 +0000

    relesing rtMedia 3.7.5
    git-svn-id: https://plugins.svn.wordpress.org/buddypress-media/trunk@972134 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- app/helper/RTMediaAddon.php
+++ app/helper/RTMediaAddon.php
@@ -68,16 +68,17 @@ if ( ! class_exists( 'RTMediaAddon' ) ){
 			$tabs = array();
 			global $rtmedia_admin;
 			$tabs[] = array(
-				'title' => __( 'Audio/Video  Encoding', 'rtmedia' ),
-				'name' => __( 'Audio/Video  Encoding', 'rtmedia' ),
-				'href' => '#rtm-services',
-				'callback' => array( $rtmedia_admin->rtmedia_encoding, 'encoding_service_intro' )
-			);
-			$tabs[] = array(
 				'title' => __( 'Plugins', 'rtmedia' ),
 				'name' => __( 'Plugins', 'rtmedia' ),
 				'href' => '#rtm-plugins',
 				'callback' => array( $this, 'plugins_content' )
+			);
+
+			$tabs[] = array(
+				'title' => __( 'Audio/Video  Encoding', 'rtmedia' ),
+				'name' => __( 'Audio/Video  Encoding', 'rtmedia' ),
+				'href' => '#rtm-services',
+				'callback' => array( $rtmedia_admin->rtmedia_encoding, 'encoding_service_intro' )
 			);
 
 			/*			$tabs[] = array(
@@ -141,13 +142,23 @@ if ( ! class_exists( 'RTMediaAddon' ) ){
 			$img_src = RTMEDIA_URL .'app/assets/img/';
 			$addons = array(
 					array(
+						'title' => __( 'rtMedia Photo Watermark', 'rtmedia' ),
+						'img_src' => $img_src.'rtmedia-watermark-240x184.png',
+						'product_link' => 'http://rtcamp.com/store/rtmedia-photo-watermark/?utm_source=dashboard&utm_medium=plugin&utm_campaign=buddypress-media',
+						'desc' => '<p>' . __( 'rtMedia Photo Watermark add-on let you add text or copyright on your images uploaded using rtMedia.', 'rtmedia' ) . '</p>
+						<p><strong>' . __( 'Important', 'rtmedia' ) . ':</strong> ' . __( 'You need to have either ImageMagick or GD library installed on your server for this addon to work.', 'rtmedia' ) . '</p>',
+						'price' => '$49',
+						'demo_link' => 'http://demo.rtcamp.com/rtmedia/?utm_source=dashboard&utm_medium=plugin&utm_campaign=buddypress-media',
+						'buy_now' => 'http://rtcamp.com/store/?utm_source=dashboard&utm_medium=plugin&utm_campaign=buddypress-media&add-to-cart=70305'
+					),
+					array(
 						'title' => __( 'rtMedia Photo Tagging', 'rtmedia' ),
 						'img_src' => $img_src.'rtmedia-phototagging-240x184.png',
 						'product_link' => 'http://rtcamp.com/store/buddypress-media-photo-tagging/?utm_source=dashboard&utm_medium=plugin&utm_campaign=buddypress-media',
 						'desc' => '<p>' . __( 'rtMedia Photo Tagging add-on enables tagging on photos uploaded using BuddyPress Media.', 'rtmedia' ) . '</p>
 						<p><strong>' . __( 'Important', 'rtmedia' ) . ':</strong> ' . __( 'You need to have ImageMagick installed on your server for this addon to work.', 'rtmedia' ) . '</p>',
 						'price' => '$49',
-						'demo_link' => 'http://demo.rtcamp.com/buddypress-media/?utm_source=dashboard&utm_medium=plugin&utm_campaign=buddypress-media',
+						'demo_link' => 'http://demo.rtcamp.com/rtmedia/?utm_source=dashboard&utm_medium=plugin&utm_campaign=buddypress-media',
 						'buy_now' => 'http://rtcamp.com/store/?utm_source=dashboard&utm_medium=plugin&utm_campaign=buddypress-media&add-to-cart=37506'
 					),
 					array(
@@ -157,7 +168,7 @@ if ( ! class_exists( 'RTMediaAddon' ) ){
 						'desc' => '<p>' . __( 'rtMedia Instagram adds Instagram like filters to images uploaded with rtMedia.', 'rtmedia' ) . '</p>
 						<p><strong>' . __( 'Important', 'rtmedia' ) . ':</strong> ' . __( 'You need to have ImageMagick installed on your server for this addon to work.', 'rtmedia' ) . '</p>',
 						'price' => '$49',
-						'demo_link' => 'http://demo.rtcamp.com/buddypress-media/?utm_source=dashboard&utm_medium=plugin&utm_campaign=buddypress-media',
+						'demo_link' => 'http://demo.rtcamp.com/rtmedia/?utm_source=dashboard&utm_medium=plugin&utm_campaign=buddypress-media',
 						'buy_now' => 'http://rtcamp.com/store/?utm_source=dashboard&utm_medium=plugin&utm_campaign=buddypress-media&add-to-cart=34379'
 					),
 					array(
