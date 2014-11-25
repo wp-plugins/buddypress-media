761ca742fabfeb3674b89403c8f352a6ea3a2ef2
rtcamp <rtcamp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-25 14:16:12 +0000

    Tagging version 3.7.20
    git-svn-id: https://plugins.svn.wordpress.org/buddypress-media/tags/3.7.20@1032521 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- app/assets/js/rtMedia.backbone.js
+++ app/assets/js/rtMedia.backbone.js
@@ -58,7 +58,8 @@ jQuery( function ( $ ) {
             "guid": false,
             "width": 0,
             "height": 0,
-            "rt_permalink": false
+            "rt_permalink": false,
+            "duration": "0:00"
             //			"next"			: -1,
             //			"prev"			: -1
         }
@@ -1106,7 +1107,7 @@ jQuery( document ).ready( function ( $ ) {
 
 
     } );
-    $( document ).on( "click", '.rtmedia-featured', function ( e ) {
+    $( document ).on( "click", '.rtmedia-featured, .rtmedia-group-featured', function ( e ) {
         e.preventDefault();
         var that = this;
         $( this ).attr( 'disabled', 'disabled' );
